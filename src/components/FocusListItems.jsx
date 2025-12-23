659ac95680b39e3be95e5952e5a60730c6eaf763
KentDoCode <villalunkent03@gmail.com>
2025-12-23 14:43:37 +0800

    Add behavior of the save focus and displaying the list of focus in todays focus list

--- src/components/FocusListItems.jsx
+++ src/components/FocusListItems.jsx
@@ -1,28 +1,25 @@
-const FocusListItems = () => {
-    return (
-        <>
-            <div className="flex flex-col gap-3 lg:col-span-1 md:col-span-1 col-span-2 md:order-3 ">
-                <h2 className="text-2xl font-semibold">Today's Targets</h2>
-                <div className="flex flex-col gap-4">
-                    
-                    <div className="w-full bg-white p-4 shadow-lg rounded-xl transform hover:-translate-y-1 hover:shadow-xl transition-all hover:border-b-violet-700 hover:border-b-3 hover:cursor-pointer pb-8">
-                        <h3 className="text-xl font-semibold">Make React Hooks</h3>
-                       
-                    </div>
+const FocusListItems = ({ focusList }) => {
+  return (
+    <>
+      <div className="flex flex-col gap-3 lg:col-span-1 md:col-span-1 col-span-2 md:order-3 ">
+        <h2 className="text-2xl font-semibold">Today's Targets</h2>
+        <div className="flex flex-col gap-4">
+          {focusList.map((f) => {
+            return (
+              <div
+                className="w-full bg-white p-4 shadow-lg rounded-xl transform hover:-translate-y-1 hover:shadow-xl transition-all hover:border-b-violet-700 hover:border-b-3 hover:cursor-pointer pb-8"
+                key={f.id}
+              >
+                <h3 className="text-xl font-semibold">{f.focusName}</h3>
+              </div>
+            );
+          })}
 
-                    <div className="w-full bg-white p-4 shadow-lg rounded-xl transform hover:-translate-y-1 hover:shadow-xl transition-all hover:border-b-violet-700 hover:border-b-3 hover:cursor-pointer pb-8">
-                        <h3 className="text-xl font-semibold">Make Custom and Reusable Hooks</h3>
-                        
-                    </div>
 
-                    <div className="w-full bg-white p-4 shadow-lg rounded-xl transform hover:-translate-y-1 hover:shadow-xl transition-all hover:border-b-violet-700 hover:border-b-3 hover:cursor-pointer pb-8">
-                        <h3 className="text-xl font-semibold">Learn about React Components</h3>
-                       
-                    </div>
-                </div>
-            </div>
-        </>
-    );
-}
- 
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default FocusListItems;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,19 +3,44 @@ import EntryContent from "../components/EntryContent";
 import FocusListItems from "../components/FocusListItems";
 import RecenEntriesList from "../components/RecentEntriesList";
 import TagChip from "../components/TagChip";
+import { useState } from "react";
 
 const Dashboard = () => {
+  const [focusText, setFocusText] = useState("");
+  const [focusList, setFocusList] = useState([])
+
+  
+
+  const addFocus = () => {
+    console.log(focusText);
+
+    const focus = {
+      id: focusList.length === 0 ? 1 : focusList[focusList.length - 1].id + 1,
+      focusName: focusText,
+    }
+
+    setFocusList([...focusList, focus])
+    setFocusText("")
+    console.log(focusList)
+    
+  };
+
+  
+
   return (
     <>
       <div className="grid grid-cols-1 grid-rows-auto gap-10 md:grid-cols-2 md:grid-rows-[auto, auto] ">
-      {/* <div className="grid lg:grid-rows-[auto_auto_1fr_1fr_1fr] gap-5 md:grid-cols-2 grid-cols-1"> */}
         <div className="flex flex-col gap-1 md:col-span-2">
           <h1 className="text-4xl font-semibold">Dashboard</h1>
           <p className="text-lg">Your learning overview.</p>
         </div>
         <div className="flex flex-col gap-5">
-          <Card />
-          <FocusListItems />
+          <Card
+            value={focusText}
+            onChange={(event) => setFocusText(event.target.value)}
+            onSave={addFocus}
+          />
+          <FocusListItems focusList={focusList}/>
         </div>
         <div className="flex flex-col gap-5">
           <TagChip />
